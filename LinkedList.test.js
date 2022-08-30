ffc6beb5cc4cd0eaf93bb1c3e455402103bb9a77
Tait Weinert <tait.weinert@outlook.com>
2022-08-30 19:46:53 +0930

    tests rewrite

--- LinkedList.test.js
+++ LinkedList.test.js
@@ -1,12 +1,33 @@
 import Node from "./Node";
 import LinkedList from "./LinkedList";
 
-let headNode = new Node("Head");
+let headNode = new Node("This is the first node");
 const testList = new LinkedList(headNode);
 
 describe("Linked List methods", () => {
   it("append()", () => {
     testList.append("This was appended");
-    expect(testList.tail()).toBe("This was appended");
+    expect(testList.tail().value).toBe("This was appended");
+  });
+
+  it("prepend()", () => {
+    testList.prepend("This was prepended");
+    expect(testList.head().value).toBe("This was prepended");
+  });
+
+  it("size()", () => {
+    expect(testList.size()).toBe(3);
+  });
+
+  it("head()", () => {
+    expect(testList.head().value).toBe("This was prepended");
+  });
+
+  it("tail()", () => {
+    expect(testList.tail().value).toBe("This was appended");
+  });
+
+  it("at()", () => {
+    expect(testList.at(1).value).toBe("This is the first node");
   });
 });
